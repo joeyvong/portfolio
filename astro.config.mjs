046ecd860e8d36fcba1f6ddfafecd38a47e52f7c
joey.vongpha@gmail.com <joey.vongpha@gmail.com>
2026-03-21 11:01:39 -0600

    ref: actually removed tailwindcss packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwindcss from '@tailwindcss/vite';
-
 import icon from 'astro-icon';
+
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
-
-  integrations: [icon()]
+  integrations: [icon(), mdx()]
 });
